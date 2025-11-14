2a54efd66882b50d727264241a85988b0cd423d6
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2025-11-14 21:04:48 +1000

    move index.html, update folder name to csomething I like a little better

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
     return {
         dir: {
             input: 'src',
-            includes: '_includes',
+            includes: '_assets',
             output: '_site',
         },
         templateFormats: ['md', 'njk', 'html'],
