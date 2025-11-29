947142b0534a7573af1394bfccb60be7c338f317
henryisbackonline <177583903+henryisbackonline@users.noreply.github.com>
2025-11-30 08:44:47 +1000

    remove comments

--- .eleventy.js
+++ .eleventy.js
@@ -1,19 +1,18 @@
 export default async function(eleventyConfig) {
     //Pass images through to _site
-    eleventyConfig.addPassthroughCopy("src/img")
+    eleventyConfig.addPassthroughCopy("/src/img")
 
     //Copy theme folder to _site/theme
-    eleventyConfig.addPassthroughCopy("theme")
+    eleventyConfig.addPassthroughCopy("/theme")
 
     //Alias the layout themes to make my markdown frontmatter more readable
     eleventyConfig.addLayoutAlias("base", "_layouts/base.liquid")
-    // eleventyConfig.addLayoutAlias("post", "_layouts/post.liquid") // don't have one of these yet, but I will soon
+    // eleventyConfig.addLayoutAlias("post", "_layouts/post.liquid")
 
     return {
         dir: {
             input: "src",
             includes: "_includes",
-            layouts: "_layouts",
             output: "_site",
         },
         templateFormats: ["md", "njk", "html"],
